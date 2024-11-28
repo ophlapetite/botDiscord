a536ebd5e4f2ea26d8895a13bdf54ad8f28e9b2d
Ophélie Cluzel <60747038+ophlapetite@users.noreply.github.com>
2024-11-28 23:17:19 +0100

    Update main.py avec token secret github

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+# Charge les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Récupère le token du bot
+token = os.getenv('TOKEN_BOT_DISCORD')
+
+if token is None:
+    print("Le token n'a pas été trouvé dans les variables d'environnement.")
+else:
+    print("Token chargé avec succès !")
+
+# Initialisation du bot
 intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents = intents)
-client = discord.Client(intents = discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-#@bot.command()
-#async def bonjour(ctx):
-#  await ctx.send(f"Bonjour {ctx.author}!")
-
-#@bot.event
-#async def on_message(message):
-#  if not message.author.bot:
-#    await message.channel.send("Bonjour c'est le bot!")
-
-
-# Événement on_member_join
+# Votre code de bot ici
 @bot.event
 async def on_member_join(member):
-    
-      welcome_channel = discord.utils.get(member.guild.text_channels, name="bienvenue-et-règles")
-      if welcome_channel is not None:
+    welcome_channel = discord.utils.get(member.guild.text_channels, name="bienvenue-et-règles")
+    if welcome_channel is not None:
         await welcome_channel.send(f"Bienvenue sur le serveur de NSI {member.mention}, n'oublie pas de te renommer! 🔫")
 
-  
-token = os.environ['TOKEN_BOT_DISCORD']
 bot.run(token)
